ee9d8cb76222750cb56079fff809b25eb84d0530
Lialil <46187567+Lialil@users.noreply.github.com>
2018-12-28 19:44:45 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 $(document).ready(function () {
 	var changeBlock = function() {
 		var heightWithScrollBars = $(window).height();
 		var widthWithScrollBars = $(window).width();
 		if (heightWithScrollBars < widthWithScrollBars ) {
 			var x2 = $('.pole').height();
-			$('.pole').html("<h1>Hi!</h1>");
-			
+			$('.pole').html("<h1>Hi!</h1>");			
 			$('.pole').css({'width': x2 + 'px'});
 			$('.block').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});						
 		}		
@@ -18,30 +16,5 @@ $(document).ready(function () {
 	};
 
 	$(window).resize(changeBlock);
-
 	changeBlock();	
 })
-=======
-$(document).ready(function () {
-	var changeBlock = function() {
-		var heightWithScrollBars = $(window).height();
-		var widthWithScrollBars = $(window).width();
-		if (heightWithScrollBars < widthWithScrollBars ) {
-			var x2 = $('.pole').height();
-			$('.pole').html("<h1>Hi!</h1>");
-			
-			$('.pole').css({'width': x2 + 'px'});
-			$('.block').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});						
-		}		
-		else {
-			var x2 = $('.block1').width();
-			$('.pole').html("<h1>Hi</h1>");
-			$('.pole').css({'height': x2 + 'px'});
-		}
-	};
-
-	$(window).resize(changeBlock);
-
-	changeBlock();	
-})
->>>>>>> 2000a728995fb6f8af680a61a52b0e74afd5ab5d
