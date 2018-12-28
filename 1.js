92dcba3f7b29ed00eb1acc1734cc911cf5086a07
Lialil <46187567+Lialil@users.noreply.github.com>
2018-12-28 19:09:13 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+$(document).ready(function () {
+	var changeBlock = function() {
+		var heightWithScrollBars = $(window).height();
+		var widthWithScrollBars = $(window).width();
+		if (heightWithScrollBars < widthWithScrollBars ) {
+			var x2 = $('.pole').height();
+			$('.pole').html("<h1>Hi!</h1>");
+			
+			$('.pole').css({'width': x2 + 'px'});
+			$('.block').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});						
+		}		
+		else {
+			var x2 = $('.block1').width();
+			$('.pole').html("<h1>Hi</h1>");
+			$('.pole').css({'height': x2 + 'px'});
+		}
+	};
+
+	$(window).resize(changeBlock);
+
+	changeBlock();	
+})
+=======
+$(document).ready(function () {
+	var changeBlock = function() {
+		var heightWithScrollBars = $(window).height();
+		var widthWithScrollBars = $(window).width();
+		if (heightWithScrollBars < widthWithScrollBars ) {
+			var x2 = $('.pole').height();
+			$('.pole').html("<h1>Hi!</h1>");
+			
+			$('.pole').css({'width': x2 + 'px'});
+			$('.block').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});						
+		}		
+		else {
+			var x2 = $('.block1').width();
+			$('.pole').html("<h1>Hi</h1>");
+			$('.pole').css({'height': x2 + 'px'});
+		}
+	};
+
+	$(window).resize(changeBlock);
+
+	changeBlock();	
+})
+>>>>>>> 2000a728995fb6f8af680a61a52b0e74afd5ab5d
