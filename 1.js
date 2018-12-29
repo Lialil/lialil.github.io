b7bbf226acca6f0da984c1d5db3d77393e59b65b
Liali <gayde8015@gmail.com>
2018-12-29 22:45:48 +0700

    New 1.css

--- 1.js
+++ 1.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
 	var changeBlock = function() {
-		var heightWithScrollBars = $(window).height();
-		var widthWithScrollBars = $(window).width();
+		var heightWithScrollBars = $(window).outerHeight(true);
+		var widthWithScrollBars = $(window).outerWidth(true);
+		console.log(heightWithScrollBars, widthWithScrollBars);
 		if (heightWithScrollBars < widthWithScrollBars ) {
-			var x2 = $('.pole').height();
-			$('.pole').html("<h1>Hi!</h1>");			
-			$('.pole').css({'width': x2 + 'px'});
-			$('.block').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});						
+			$('body').removeClass().addClass('horisontal');
+			var x2 = $('.middle').height();						
+			$('.middle')
+			.css({'width': x2 + 'px'});
+			$('.out').css('width','').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});						
 		}		
 		else {
-			var x2 = $('.block1').width();
-			$('.pole').html("<h1>Hi</h1>");
-			$('.pole').css({'height': x2 + 'px'});
+			$('body').removeClass().addClass('vertical');
+			$('.middle').css('width', '');
+			var x1 = $('.middle').width();
+			$('.middle').css('height','').css({'height': x1 + 'px'});
+			$('.out').css('width', '');
+		
+			
+			
 		}
 	};
 
