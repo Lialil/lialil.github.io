bf8d4c50daf637b5ea259a2318539b965cf52fdc
Liali <gayde8015@gmail.com>
2018-12-30 05:47:54 +0700

    deleted 2.css and 3.css, rename 4.css to 1.css

--- 1.js
+++ 1.js
@@ -6,22 +6,19 @@ $(document).ready(function () {
 		if (heightWithScrollBars < widthWithScrollBars ) {
 			$('body').removeClass().addClass('horisontal');
 			var x2 = $('.middle').height();						
-			$('.middle')
-			.css({'width': x2 + 'px'});
-			$('.out').css('width','').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});						
+			$('.middle').css({'width': x2 + 'px'});
+			$('.out').css('width','').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});
+			$('.out').css('height','');							
 		}		
 		else {
 			$('body').removeClass().addClass('vertical');
 			$('.middle').css('width', '');
 			var x1 = $('.middle').width();
 			$('.middle').css('height','').css({'height': x1 + 'px'});
-			$('.out').css('width', '');
-		
-			
-			
+			$('.out').css('width', '');	
+			$('.out').css({'height':(heightWithScrollBars - widthWithScrollBars)/2});		
 		}
 	};
-
 	$(window).resize(changeBlock);
 	changeBlock();	
 })
