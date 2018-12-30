e3bd4ee79ec6abf3c288c5bab37965dccdeb902b
Liali <gayde8015@gmail.com>
2018-12-31 02:16:37 +0700

    add span

--- 1.js
+++ 1.js
@@ -5,18 +5,22 @@ $(document).ready(function () {
 		console.log(heightWithScrollBars, widthWithScrollBars);
 		if (heightWithScrollBars < widthWithScrollBars ) {
 			$('body').removeClass().addClass('horisontal');
+			$('.middle').css('height', '');
 			var x2 = $('.middle').height();						
 			$('.middle').css({'width': x2 + 'px'});
-			$('.out').css('width','').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'});
-			$('.out').css('height','');							
+			$('.out').css('width','').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'}).css('height','');
+			var x2 = $('.out').width();
+			$('.span').css('width', '').css({'width':(widthWithScrollBars -heightWithScrollBars - x2*2)/2});									
 		}		
+		
 		else {
 			$('body').removeClass().addClass('vertical');
 			$('.middle').css('width', '');
 			var x1 = $('.middle').width();
 			$('.middle').css('height','').css({'height': x1 + 'px'});
-			$('.out').css('width', '');	
-			$('.out').css({'height':(heightWithScrollBars - widthWithScrollBars)/2});		
+			$('.out').css('width', '').css({'height':(heightWithScrollBars - widthWithScrollBars)/2});
+			var x2 = $('.out').height();
+			$('.span').css('height', '').css({'height':(heightWithScrollBars - widthWithScrollBars - x2*2)/2});
 		}
 	};
 	$(window).resize(changeBlock);
