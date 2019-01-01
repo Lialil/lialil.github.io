96db1b3ba81bc81d08cf1444260b4493671fc018
Liali <gayde8015@gmail.com>
2019-01-02 00:24:15 +0700

    Delete class span

--- 1.js
+++ 1.js
@@ -1,26 +1,22 @@
 $(document).ready(function () {
-	var changeBlock = function() {
-		var heightWithScrollBars = $(window).outerHeight(true);
-		var widthWithScrollBars = $(window).outerWidth(true);
-		console.log(heightWithScrollBars, widthWithScrollBars);
+	let changeBlock = function() {
+		let heightWithScrollBars = $(window).outerHeight(true);
+		let widthWithScrollBars = $(window).outerWidth(true);
+		$('body').removeClass();
+		$('.middle').css('width', '').css('height', '');
+		$('.span').css('width', '').css('height', '');
+		$('.out').css('width', '').css('height', '');
 		if (heightWithScrollBars < widthWithScrollBars ) {
-			$('body').removeClass().addClass('horisontal');
-			$('.middle').css('height', '');
-			var x2 = $('.middle').height();						
-			$('.middle').css({'width': x2 + 'px'});
-			$('.out').css('width','').css({'width': (widthWithScrollBars -heightWithScrollBars)/2 + 'px'}).css('height','');
-			var x2 = $('.out').width();
-			$('.span').css('width', '').css({'width':(widthWithScrollBars -heightWithScrollBars - x2*2)/2});									
-		}		
-		
+			$('body').addClass('horisontal');
+			let x = $('.middle').height();	
+			$('.middle').css({'width': x});
+			$('.out').css({ 'width': (widthWithScrollBars -heightWithScrollBars)/2 });									
+		}				
 		else {
-			$('body').removeClass().addClass('vertical');
-			$('.middle').css('width', '');
-			var x1 = $('.middle').width();
-			$('.middle').css('height','').css({'height': x1 + 'px'});
-			$('.out').css('width', '').css({'height':(heightWithScrollBars - widthWithScrollBars)/2});
-			var x2 = $('.out').height();
-			$('.span').css('height', '').css({'height':(heightWithScrollBars - widthWithScrollBars - x2*2)/2});
+			$('body').addClass('vertical');
+			let x = $('.middle').width();			
+			$('.middle').css({'height': x});
+			$('.out').css({'height':(heightWithScrollBars - widthWithScrollBars)/2 });			
 		}
 	};
 	$(window).resize(changeBlock);
