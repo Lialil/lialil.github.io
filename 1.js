bcd3c9eec0dba43fb19d764518dd0c1fb0ca82fe
Liali <gayde8015@gmail.com>
2019-01-09 19:43:22 +0700

    changing a laptop

--- 1.js
+++ 1.js
@@ -1,24 +1,36 @@
-$(document).ready(function () {
-	let changeBlock = function() {
-		let heightWithScrollBars = $(window).outerHeight(true);
-		let widthWithScrollBars = $(window).outerWidth(true);
-		$('body').removeClass();
-		$('.middle').css('width', '').css('height', '');
-		$('.span').css('width', '').css('height', '');
-		$('.out').css('width', '').css('height', '');
-		if (heightWithScrollBars < widthWithScrollBars ) {
-			$('body').addClass('horisontal');
-			let x = $('.middle').height();	
-			$('.middle').css({'width': x});
-			$('.out').css({ 'width': (widthWithScrollBars -heightWithScrollBars)/2 });									
-		}				
-		else {
-			$('body').addClass('vertical');
-			let x = $('.middle').width();			
-			$('.middle').css({'height': x});
-			$('.out').css({'height':(heightWithScrollBars - widthWithScrollBars)/2 });			
-		}
-	};
-	$(window).resize(changeBlock);
-	changeBlock();	
-})
+$(document).ready(() => {
+  const changeBlock = function () {
+    const $w = $(window);
+
+
+    const $out = $('.out');
+
+
+    const $body = $('body');
+
+
+    const $middle = $('.middle');
+
+    const widthWithScrollBars = $w.outerWidth(true);
+
+
+    const heightWithScrollBars = $w.outerHeight(true);
+
+    $body.removeClass();
+
+    if (heightWithScrollBars < widthWithScrollBars) {
+      $body.addClass('horisontal');
+      const x = $middle.height();
+      $middle.css({ width: x, height: '' });
+      $out.css({ width: (widthWithScrollBars - heightWithScrollBars) / 2, height: '' });
+    } else {
+      $body.addClass('vertical');
+      const x = $middle.width();
+      $middle.css({ height: x, width: '' });
+      $out.css({ height: (heightWithScrollBars - widthWithScrollBars) / 2, width: '' });
+    }
+  };
+
+  $(window).resize(changeBlock);
+  changeBlock();
+});
