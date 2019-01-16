975d08bc6fc10a080cfd319499dd92d2430601df
Liali <gayde8015@gmail.com>
2019-01-16 16:09:23 +0700

    renamig from pole to field

--- game.js
+++ game.js
@@ -6,69 +6,73 @@ const choise = ['l', 'r', 'u', 'd', 'q'];
 // let choise  = ['left', 'right','up', 'down', 'exit'];
 
 const rotation = [];
-const pole = [];
+const playingField = [];
 
 const n = 4;
 
-let help; let i; let k; let j; let l; let k1; let q;
-// const pole = {
+let help; let i; let k; let j; let l; let m; let q;
+// const field = {
 //   0: 7, 1: 2, 2: 5, 3: 3, 4: 9, 5: 3, 6: 8, 7: 1, 8: 2, 9: 1, 10: 9, 11: 5, 12: 2, 13: 5, 14: 3, 15: 6,
 // };
-
-
+const d = 0;
 for (i = 0; i < n * n; i += 1) {
-  pole[i] = 0;
+  playingField[i] = d;
+  // d=d+1;
 }
-const rand = Math.floor(Math.random() * pole.length);
-pole[rand] = 2;
-const rand2 = Math.floor(Math.random() * pole.length);
-pole[rand2] = 2;
 
-function algorithm(pole) {
+function rand() {
+  const random = Math.floor(Math.random() * playingField.length);
+  return random;
+}
+
+playingField[rand()] = 2;
+playingField[rand()] = 2;
+
+function algorithm(field) {
   let check = 0;
   for (i = 0; i < n * n; i += n) {
     for (j = i; j < i + n; j += 1) {
-      if (pole[j] === pole[j + 1] && pole[j] !== 0 && j + 1 < i + n) {
+      if (field[j] === field[j + 1] && field[j] !== 0 && j + 1 < i + n) {
         check += 1;
-      } else if (pole[j - 1] === 0 && j - 1 > i) {
+      } else if (field[j - 1] === 0 && j - 1 > i) {
       	check += 1;
       }
     }
   }
-  if (pole[0] === 0) { help = true; }
+  if (field[0] === 0) { help = true; }
   for (i = 0; i < n * n; i += n) {
     for (j = i; j < i + n; j += 1) {
       let w = j + 1;
-      if (pole[j] === 0) {
-        if (pole[j + 1] === 0 && j + 1 < i + n) {
-          while (pole[w] === 0 && w < i + n) {
+      if (field[j] === 0) {
+        if (field[j + 1] === 0 && j + 1 < i + n) {
+          while (field[w] === 0 && w < i + n) {
             w += 1;
           }
           if (w < i + n) {
             for (k = j, q = w; q < i + n; q += 1, k += 1) {
-              pole[k] = pole[q];
+              field[k] = field[q];
             }
-            for (k1 = i + n - 1; k1 >= i + n - w + j; k1 -= 1) {
-              pole[k1] = 0;
+            for (m = i + n - 1; m >= i + n - w + j; m -= 1) {
+              field[m] = 0;
             }
           }
         } else {
           for (k = j; k < i + n - 1; k += 1) {
-            pole[k] = pole[k + 1];
+            field[k] = field[k + 1];
           }
-          pole[i + n - 1] = 0;
+          field[i + n - 1] = 0;
         }
       }
     }
   }
   for (i = 0; i < n * n; i += n) {
     for (j = i; j < i + n; j += 1) {
-      if (pole[j] === pole[j + 1] && pole[j] !== 0 && j + 1 < i + n) {
-        pole[j] = pole[j] * 2;
+      if (field[j] === field[j + 1] && field[j] !== 0 && j + 1 < i + n) {
+        field[j] = field[j] * 2;
         for (k = j + 1; k < i + n - 1; k += 1) {
-          pole[k] = pole[k + 1];
+          field[k] = field[k + 1];
         }
-        pole[i + n - 1] = 0;
+        field[i + n - 1] = 0;
       }
     }
   }
@@ -76,22 +80,21 @@ function algorithm(pole) {
   if (check > 0 || help === true) {
     const arr = [];
     for (i = 0; i < n * n; i += 1) {
-      if (pole[i] === 0) {
+      if (field[i] === 0) {
         arr.push(i);
       }
     }
-    const rand3 = Math.floor(Math.random() * arr.length);
-    pole[arr[rand3]] = 2;
+    field[arr[rand()]] = 2;
   }
 
   let over = 0;
   for (i = 0; i < n * n; i += n) {
     for (j = i; j < i + n; j += 1) {
-      if (pole[j] === pole[j + 1] && pole[j] !== 0 && j + 1 < i + n) {
+      if (field[j] === field[j + 1] && field[j] !== 0 && j + 1 < i + n) {
         over += 1;
-      } else if (pole[j] === pole[j + n] && j < n * n - n && pole[j] !== 0) {
+      } else if (field[j] === field[j + n] && j < n * n - n && field[j] !== 0) {
       	over += 1;
-      } else if (pole[j] === 0) {
+      } else if (field[j] === 0) {
       	over += 1;
       }
     }
@@ -103,63 +106,63 @@ function algorithm(pole) {
   }
 }
 
-function showing(pole) {
+function showing(field) {
   for (i = 0; i < n * n; i += n) {
     const show = [];
     for (j = i; j < i + n; j += 1) {
-      show.push(pole[j]);
+      show.push(field[j]);
     }
     console.log(show.join('  '));
   }
 }
 
-function mirror(rotation, pole) {
+function mirror(rotation, field) {
   for (i = 0; i < n * n; i += n) {
     for (j = i, k = i + n - 1; j < i + n, k >= i; k -= 1, j += 1) {
-    	rotation[j] = pole[k];
+    	rotation[j] = field[k];
     }
   }
 }
-function roll() {
+function roll(field) {
   for (i = k = 0; i < n, k < n * n; i += 1, k += n) {
     for (j = i - n, l = k; j < i + n * n - n, l < k + n; j += n, l += 1) {
-      rotation[j + n] = pole[l];
+      rotation[j + n] = field[l];
     }
   }
 }
-function comeBack() {
+function comeBack(field) {
   for (i = k = 0; i < n, k < n * n; i += 1, k += n) {
     for (j = i - n, l = k; j < i + n * n - n, l < k + n; j += n, l += 1) {
-      pole[l] = rotation[j + n];
+      field[l] = rotation[j + n];
     }
   }
 }
 console.log('Original');
-showing(pole);
+showing(playingField);
 console.log('You can choose:', choise.join(', '));
 rl.setPrompt('Your answer: ');
 rl.prompt();
 rl.on('line', (line) => {
   switch (line) {
     case choise[0]:
-      algorithm(pole);
+      algorithm(playingField);
       break;
     case choise[1]:
-      mirror(rotation, pole);
+      mirror(rotation, playingField);
       algorithm(rotation);
-      mirror(pole, rotation);
+      mirror(playingField, rotation);
       break;
     case choise[2]:
-      roll();
+      roll(playingField);
       algorithm(rotation);
-      comeBack();
+      comeBack(playingField);
       break;
     case choise[3]:
-      roll();
-      mirror(pole, rotation);
-      algorithm(pole);
-      mirror(rotation, pole);
-      comeBack();
+      roll(playingField);
+      mirror(playingField, rotation);
+      algorithm(playingField);
+      mirror(rotation, playingField);
+      comeBack(playingField);
       break;
     case choise[4]:
       rl.close();
@@ -168,6 +171,6 @@ rl.on('line', (line) => {
       console.log('Incorrect answer. Please try again)');
       break;
   }
-  showing(pole);
+  showing(playingField);
   rl.prompt();
 }).on('close', () => { process.exit(0); });
