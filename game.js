6372e5b14de4a9e51aaf611cdff4fe4da398f8ca
Liali <gayde8015@gmail.com>
2019-01-16 22:59:17 +0700

    adding function newField

--- game.js
+++ game.js
@@ -4,42 +4,52 @@ const rl = readline.createInterface(process.stdin, process.stdout);
 
 const choise = ['l', 'r', 'u', 'd', 'q'];
 // let choise  = ['left', 'right','up', 'down', 'exit'];
-
-const rotation = [];
 const playingField = [];
-
+const rotation = [];
 const n = 4;
 
 let help; let i; let k; let j; let l; let m; let q;
-// const field = {
-//   0: 7, 1: 2, 2: 5, 3: 3, 4: 9, 5: 3, 6: 8, 7: 1, 8: 2, 9: 1, 10: 9, 11: 5, 12: 2, 13: 5, 14: 3, 15: 6,
+// const playingField = {
+//   0: 0, 1: 4, 2: 2, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0,
 // };
-const d = 0;
-for (i = 0; i < n * n; i += 1) {
-  playingField[i] = d;
-  // d=d+1;
-}
 
-function rand() {
-  const random = Math.floor(Math.random() * playingField.length);
+function rand(field) {
+  const random = Math.floor(Math.random() * field.length);
   return random;
 }
 
-playingField[rand()] = 2;
-playingField[rand()] = 2;
+function newField() {
+  const d = 0;
+  for (i = 0; i < n * n; i += 1) {
+	  playingField[i] = d;
+	  // d=d+1;
+  }
+  const r = rand(playingField);
+  const first = rand(playingField);
+  let second = first;
+  playingField[first] = 2;
+  while (second == first) {
+    second = rand(playingField);
+  }
+  playingField[second] = 2;
+}
 
 function algorithm(field) {
+  if (field[0] === 0) { help = true; }
   let check = 0;
   for (i = 0; i < n * n; i += n) {
     for (j = i; j < i + n; j += 1) {
       if (field[j] === field[j + 1] && field[j] !== 0 && j + 1 < i + n) {
         check += 1;
-      } else if (field[j - 1] === 0 && j - 1 > i) {
+        console.log('be');
+      } else if (field[j] != 0 && field[j - 1] == 0) {
       	check += 1;
+      	console.log('beeeee');
       }
     }
   }
-  if (field[0] === 0) { help = true; }
+  console.log(check);
+
   for (i = 0; i < n * n; i += n) {
     for (j = i; j < i + n; j += 1) {
       let w = j + 1;
@@ -65,6 +75,7 @@ function algorithm(field) {
       }
     }
   }
+
   for (i = 0; i < n * n; i += n) {
     for (j = i; j < i + n; j += 1) {
       if (field[j] === field[j + 1] && field[j] !== 0 && j + 1 < i + n) {
@@ -77,15 +88,21 @@ function algorithm(field) {
     }
   }
 
-  if (check > 0 || help === true) {
+
+  if (check > 0) {
     const arr = [];
     for (i = 0; i < n * n; i += 1) {
       if (field[i] === 0) {
         arr.push(i);
       }
     }
-    field[arr[rand()]] = 2;
+    console.log('arr', arr);
+
+    const rr = rand(arr);
+    console.log('rr', arr[rr]);
+    field[arr[rr]] = 2;
   }
+
 
   let over = 0;
   for (i = 0; i < n * n; i += n) {
@@ -102,9 +119,10 @@ function algorithm(field) {
 
   if (over === 0) {
     console.log('Game over.');
-    rl.close();
+    // rl.close();
   }
 }
+
 
 function showing(field) {
   for (i = 0; i < n * n; i += n) {
@@ -137,6 +155,8 @@ function comeBack(field) {
     }
   }
 }
+
+newField();
 console.log('Original');
 showing(playingField);
 console.log('You can choose:', choise.join(', '));
