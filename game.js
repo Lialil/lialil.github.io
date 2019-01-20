fd6188f9476e2952456d10be9fc7e3b23647e28d
Liali <gayde8015@gmail.com>
2019-01-20 20:16:17 +0700

    adding function addTwo

--- game.js
+++ game.js
@@ -2,45 +2,68 @@ const readline = require('readline');
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 
+const n = 4;
+
 const choise = ['l', 'r', 'u', 'd', 'q'];
 // const choise  = ['left', 'right','up', 'down', 'exit'];
 
-const playingField = [];
+// // const playingField = [];
+// const playingField = {
+//   0: 9, 1: 4, 2: 2, 3: 6, 4: 4, 5: 7, 6: 6, 7: 7, 8: 9, 9: 6, 10: 12, 11: 10, 12: 11, 13: 9, 14: 15, 15: 9,
+// };
+
+// const field = {
+//   0: 9, 1: 4, 2: 2, 3: 6, 4: 4, 5: 7, 6: 6, 7: 0, 8: 9, 9: 0, 10: 12, 11: 10, 12: 11, 13: 9, 14: 15, 15: 9,
+// };
+
+
 const duplicate = [];
-const n = 4;
 
-let help; let i; let k; let j; let l; let m; let q; let over;
 
-function rand(field) {
-  const random = Math.floor(Math.random() * field.length);
-  return random;
-}
-function newField() {
-  for (i = 0; i < n * n; i += 1) {
-	  playingField[i] = 0;
+let q;
+
+function newField(field) {
+  for (let i = 0; i < n * n; i += 1) {
+	  field[i] = 0;
   }
-  const first = rand(playingField);
-  let second = first;
-  playingField[first] = 2;
-  while (second == first) {
-    second = rand(playingField);
-  }
-  playingField[second] = 2;
 }
-function algorithm(field) {
-  if (field[0] === 0) { help = true; }
+
+// if (check > 0)
+function addTwo(field) {
+  const arr = [];
+  for (let i = 0; i < n * n; i += 1) {
+    if (field[i] === 0) {
+      arr.push(i);
+    }
+  }
+  const random = Math.floor(Math.random() * arr.length);
+  field[arr[random]] = 2;
+  if (field[arr[random]] == 2) {
+  	return true;
+  }
+  	return false;
+}
+
+function Check(field) {
   let check = 0;
-  for (i = 0; i < n * n; i += n) {
-    for (j = i; j < i + n; j += 1) {
+  for (let i = 0; i < n * n; i += n) {
+    for (let j = i; j < i + n; j += 1) {
       if (field[j] === field[j + 1] && field[j] !== 0 && j + 1 < i + n) {
         check += 1;
-      } else if (field[j] != 0 && field[j - 1] == 0 && j - 1 >= i) {
+      } else if (field[j] !== 0 && field[j - 1] == 0 && j - 1 >= i) {
       	check += 1;
       }
     }
   }
-  for (i = 0; i < n * n; i += n) {
-    for (j = i; j < i + n; j += 1) {
+  if (check > 0) {
+  	return true;
+  }
+  	return false;
+}
+
+function algorithm(field) {
+  for (let i = 0; i < n * n; i += n) {
+    for (let j = i; j < i + n; j += 1) {
       let w = j + 1;
       if (field[j] === 0) {
         if (field[j + 1] === 0 && j + 1 < i + n) {
@@ -48,15 +71,15 @@ function algorithm(field) {
             w += 1;
           }
           if (w < i + n) {
-            for (k = j, q = w; q < i + n; q += 1, k += 1) {
+            for (let k = j, q = w; q < i + n; q += 1, k += 1) {
               field[k] = field[q];
             }
-            for (m = i + n - 1; m >= i + n - w + j; m -= 1) {
+            for (let m = i + n - 1; m >= i + n - w + j; m -= 1) {
               field[m] = 0;
             }
           }
         } else {
-          for (k = j; k < i + n - 1; k += 1) {
+          for (let k = j; k < i + n - 1; k += 1) {
             field[k] = field[k + 1];
           }
           field[i + n - 1] = 0;
@@ -64,29 +87,24 @@ function algorithm(field) {
       }
     }
   }
-  for (i = 0; i < n * n; i += n) {
-    for (j = i; j < i + n; j += 1) {
+
+  for (let i = 0; i < n * n; i += n) {
+    for (let j = i; j < i + n; j += 1) {
       if (field[j] === field[j + 1] && field[j] !== 0 && j + 1 < i + n) {
         field[j] = field[j] * 2;
-        for (k = j + 1; k < i + n - 1; k += 1) {
+        for (let k = j + 1; k < i + n - 1; k += 1) {
           field[k] = field[k + 1];
         }
         field[i + n - 1] = 0;
       }
     }
   }
-  if (check > 0) {
-    const arr = [];
-    for (i = 0; i < n * n; i += 1) {
-      if (field[i] === 0) {
-        arr.push(i);
-      }
-    }
-    field[arr[rand(arr)]] = 2;
-  }
-  over = 0;
-  for (i = 0; i < n * n; i += n) {
-    for (j = i; j < i + n; j += 1) {
+}
+
+function gameOver(field) {
+  let over = 0;
+  for (let i = 0; i < n * n; i += n) {
+    for (let j = i; j < i + n; j += 1) {
       if (field[j] === field[j + 1] && field[j] !== 0 && j + 1 < i + n) {
         over += 1;
       } else if (field[j] === field[j + n] && j < n * n - n && field[j] !== 0) {
@@ -96,47 +114,50 @@ function algorithm(field) {
       }
     }
   }
-}
-function overCheck() {
   if (over === 0) {
     console.log('Game over.');
     rl.close();
   }
 }
+
+
 function showing(field) {
-  for (i = 0; i < n * n; i += n) {
+  for (let i = 0; i < n * n; i += n) {
     const show = [];
-    for (j = i; j < i + n; j += 1) {
+    for (let j = i; j < i + n; j += 1) {
       show.push(field[j]);
     }
     console.log(show.join('  '));
   }
 }
+
 function mirror(duplicate, field) {
-  for (i = 0; i < n * n; i += n) {
-    for (j = i, k = i + n - 1; j < i + n, k >= i; k -= 1, j += 1) {
+  for (let i = 0; i < n * n; i += n) {
+    for (let j = i, k = i + n - 1; j < i + n, k >= i; k -= 1, j += 1) {
     	duplicate[j] = field[k];
     }
   }
 }
 function rotate(field) {
-  for (i = k = 0; i < n, k < n * n; i += 1, k += n) {
-    for (j = i - n, l = k; j < i + n * n - n, l < k + n; j += n, l += 1) {
+  for (let i = k = 0; i < n, k < n * n; i += 1, k += n) {
+    for (let j = i - n, l = k; j < i + n * n - n, l < k + n; j += n, l += 1) {
       duplicate[j + n] = field[l];
     }
   }
 }
 function comeBack(field) {
-  for (i = k = 0; i < n, k < n * n; i += 1, k += n) {
-    for (j = i - n, l = k; j < i + n * n - n, l < k + n; j += n, l += 1) {
+  for (let i = k = 0; i < n, k < n * n; i += 1, k += n) {
+    for (let j = i - n, l = k; j < i + n * n - n, l < k + n; j += n, l += 1) {
       field[l] = duplicate[j + n];
     }
   }
 }
 
-newField();
-console.log('Original');
+newField(playingField);
+addTwo(playingField);
+addTwo(playingField);
 showing(playingField);
+
 console.log('You can choose:', choise.join(', '));
 rl.setPrompt('Your answer: ');
 rl.prompt();
@@ -147,7 +168,9 @@ rl.on('line', (line) => {
       break;
     case choise[1]:
       mirror(duplicate, playingField);
+      check(duplicate);
       algorithm(duplicate);
+
       mirror(playingField, duplicate);
       break;
     case choise[2]:
@@ -170,6 +193,6 @@ rl.on('line', (line) => {
       break;
   }
   showing(playingField);
-  overCheck();
+  gameOver();
   rl.prompt();
 }).on('close', () => { process.exit(0); });
