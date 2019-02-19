ae84bc49b4a465a22171d707fd8fb3204f86afd2
lialis <lialiseitbekova@gmail.com>
2019-02-19 20:29:21 +0700

    creating swipe function

--- gameInBrowser.js
+++ gameInBrowser.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const n = 8;
-  const cellSize = `${(100-n*2)/n}%`;
-  
+  const n = 4;
+  const cellSize = `${(100 - n * 2) / n}%`;
+  const fontSize = 10 - n;
+
   const game = new Game2048(n);
 
 
   const playingField = document.getElementById('playingField');
-
- const showing = function showing(field){
+  const showing = function showing(field) {
     while (playingField.firstChild) {
       playingField.removeChild(playingField.firstChild);
     }
     field.forEach((row) => {
-      row.forEach((cell)=>{
+      row.forEach((cell) => {
         const div = document.createElement('div');
-        div.className = "cell";
+        div.className = 'cell';
         if (cell !== 0) {
           div.innerHTML = cell;
         }
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
     [].forEach.call(document.querySelectorAll('.cell'), (el) => { el.style.width = cellSize; el.style.height = cellSize; });
-  }
+  };
 
   showing(game.createField());
 
@@ -41,14 +41,69 @@ document.addEventListener('DOMContentLoaded', () => {
         showing(game.handleDown());
         break;
       default:
-      break;
+        break;
     }
-  } 
-
- 
+  };
 
 
+  function swipedetect() {
+    const touchsurface = playingField;
+    let swipedir;
+    let startX;
+    let startY;
+    let distX;
+    let distY;
+    const threshold = 150;
+    const restraint = 100;
+    const allowedTime = 300;
+    let elapsedTime;
+    let startTime;
 
+    touchsurface.addEventListener('touchstart', (e) => {
+      const touchobj = e.changedTouches[0];
+      swipedir = 'none';
+      startX = touchobj.pageX;
+      startY = touchobj.pageY;
+      startTime = new Date().getTime();
+      e.preventDefault();
+    }, false);
 
+    touchsurface.addEventListener('touchmove', (e) => {
+      e.preventDefault();
+    }, false);
 
+    touchsurface.addEventListener('touchend', (e) => {
+      const touchobj = e.changedTouches[0];
+      distX = touchobj.pageX - startX;
+      distY = touchobj.pageY - startY;
+      elapsedTime = new Date().getTime() - startTime;
+      if (elapsedTime <= allowedTime) {
+        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+          swipedir = (distX < 0) ? 'left' : 'right';
+        } else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {
+          swipedir = (distY < 0) ? 'up' : 'down';
+        }
+      }
+      e.preventDefault();
+    }, false);
+  }
+
+  swipedetect((swipedir) => {
+    switch (swipedir) {
+      case 'left':
+        showing(game.handleLeft());
+        break;
+      case 'right':
+        showing(game.handleRight());
+        break;
+      case 'up':
+        showing(game.handleUp());
+        break;
+      case 'down':
+        showing(game.handleDown());
+        break;
+      default:
+        break;
+    }
+  });
 });
