a7936d811f07880a63a5050a9fffee45c7cae4ab
lialis <lialiseitbekova@gmail.com>
2019-02-21 15:13:08 +0700

    changing classes celll-4 cell-8 etc.

--- gameInBrowser.js
+++ gameInBrowser.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const cellSize = `${(100 - n * 2) / n}%`;
   const fontSize = 10 - n;
 
-  // const game = [[2,4,8,16],[32,64,128,256],[512,1024,2048,4096],[2,2,2,2]];
+  // const game = [[2,4,8,16],[32,64,128,256],[512,1024,2048,4096],[0,0,0,0,]];
 
   const game = new Game2048(n);
 
